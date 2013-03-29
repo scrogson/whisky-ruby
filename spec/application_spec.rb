c7723af48c31cbafa3d42b574331da3fc85b943e
Sonny Scroggin <scrogson@gmail.com>
2013-03-29 14:41:25 -0500

    Use the module name rather than a string; clean up whitespace

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -1,6 +1,6 @@
 require "spec_helper"
 
-describe 'Whisky' do
+describe Whisky do
 
   it "says hello" do
     get '/'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ require "rack/test"
 RSpec.configure do |config|
   config.mock_with :rspec
   config.include Rack::Test::Methods
-  
+
   def app
     Whisky::Application.new
   end
