390adfd4f4707a24b723c43b598864694258f3b9
Sonny Scroggin <scrogson@gmail.com>
2013-03-28 23:12:33 -0500

    Basic routing and controller

--- lib/whisky.rb
+++ lib/whisky.rb
@@ -1,9 +1,30 @@
 require "whisky/version"
+require "whisky/routing"
 
 module Whisky
   class Application
     def call(env)
-      [200, { "Content-Type" => "text/html" }, ["Hello Whisky!"]]
+      case env["PATH_INFO"]
+      when '/favicon.ico'
+        return [404, { "Content-Type" => "text/html" }, []]
+      when '/'
+        return [200, { "Content-Type" => "text/html" }, ["Hello Whisky!"]]
+      else
+        klass, action = get_controller_and_action(env)
+        controller = klass.new(env)
+        text = controller.send(action)
+        [200, { "Content-Type" => "text/html" }, [text]]
+      end
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -5,7 +5,12 @@ describe 'Whisky' do
   it "says hello" do
     get '/'
     last_response.should be_ok
-    last_response.body.should == 'Hello Whisky!'
+    last_response.body.should eq 'Hello Whisky!'
+  end
+
+  it "doesn't handle favicons" do
+    get '/favicon.ico'
+    last_response.status.should be 404
   end
 
 end
