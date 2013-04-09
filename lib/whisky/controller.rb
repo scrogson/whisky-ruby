272bf095516f2566b8c8c13c419b517697a2c756
Sonny Scroggin <scrogson@gmail.com>
2013-04-08 20:03:10 -0500

    Make controller_name available to the view

--- lib/whisky/controller.rb
+++ lib/whisky/controller.rb
@@ -9,17 +9,20 @@ module Whisky
       @env
     end
 
-    def render(view_name, locals = {})
-      filename = File.join("app", "views", controller_name, "#{view_name}.html.erb")
-      template = File.read(filename)
-      erb = Erubis::Eruby.new(template)
-      erb.result(locals.merge(env: env))
-    end
-
     def controller_name
       klass = self.class
       klass = klass.to_s.gsub(/Controller$/, '')
       Utils.underscore(klass)
+    end
+
+    def render(view_name, locals = {})
+      filename = File.join("app", "views", controller_name, "#{view_name}.html.erb")
+      template = File.read(filename)
+      erb = Erubis::Eruby.new(template)
+      erb.result(locals.merge(
+        env: env,
+        controller_name: controller_name
+      ))
     end
   end
 end
